45bfe4f434363a4710380d4fd6c37599f05a7659
ashu7mishra <ashu7mishra@gmail.com>
2024-09-29 09:04:54 +0530

    frontend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 
 function App() {
 
